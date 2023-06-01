74f908f6e6050bba181aa6c87b1ecc87fa4e8d71
Cyang39 <huqinyang123@qq.com>
2023-06-01 09:25:24 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
+-- 解决 Windows 上的 Bug：
+-- https://stackoverflow.com/questions/74389900
+vim.o.shellcmdflag="-c"
+
 -- local autocmd = vim.api.nvim_create_autocmd
 
 -- Auto resize panes when resizing nvim window
