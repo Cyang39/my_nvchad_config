6b39b9e9a1df5d841f73e967156c8a2519afe354
Cyang39 <huqinyang123@qq.com>
2023-06-01 17:29:03 +0800

    Add asm-lsp

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -7,7 +7,14 @@ capabilities.offsetEncoding = { "utf-16" }
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "rust_analyzer" }
+local servers = { 
+  "html",
+  "cssls",
+  "tsserver",
+  "clangd",
+  "rust_analyzer",
+  "asm_lsp"
+}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
